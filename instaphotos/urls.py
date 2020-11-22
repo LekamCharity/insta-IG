18ee3b08c6437c3fff1adb3f53574451c99a6dac
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-11-22 18:45:10 +0300

    updated the urls

--- instaphotos/urls.py
+++ instaphotos/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url,include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from instaphotos.vews import PostLikeToggle, PostLikeAPIToggle
 
 urlpatterns = [
     path('', views.signup, name='signup'),
