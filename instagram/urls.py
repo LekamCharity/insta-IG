6c2302ea51b61dd05fdc1425bcb4e5df383fbab3
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-11-22 14:36:49 +0300

    added templates and views

--- instagram/urls.py
+++ instagram/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('',include("instaphotos.urls")),
+    url('tinymce/',include('tinymce.urls')),
+    url('accounts/',include('registration.backends.simple.urls')),
+    url('logout/', views.logout, {"next_page": '/'}),
 ]
+
+
+
